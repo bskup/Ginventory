f78d5ccf55373c1524c3056131460c5605b382ae
bskup <bskup1@gmail.com>
2017-04-12 12:48:38 -0400

    Remove menu option to insert dummy data for debugging while under review

--- app/src/main/java/com/bskup/ginventory/CatalogActivity.java
+++ app/src/main/java/com/bskup/ginventory/CatalogActivity.java
@@ -194,10 +194,6 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
     public boolean onOptionsItemSelected(MenuItem item) {
         // User clicked on a menu option in the app bar overflow menu
         switch (item.getItemId()) {
-            // Respond to a click on the "Insert dummy data" menu option
-            case R.id.action_insert_dummy_data:
-                insertDummyItem();
-                return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
                 // Show confirmation dialog making sure user wants to delete all pets
